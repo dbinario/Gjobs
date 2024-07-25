551bb017f77f7527b309cf300d6435dc9a37bb29
Fernando Ruiz <dragon.binario@gmail.com>
2024-07-24 22:20:18 -0600

    se avanza en la autenticacion

--- controllers/postAutenticar.go
+++ controllers/postAutenticar.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"fmt"
 	"gjobs-back/app"
 	"gjobs-back/interfaces"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func PostAutenticar(c *gin.Context) {
@@ -29,15 +31,46 @@ func PostAutenticar(c *gin.Context) {
 
 	db.QueryRow("CALL sp_buscarEmail(?)", autenticar.Email).Scan(&count)
 
-	if count == 0 { //si email no existe
+	if count == 1 { //si email no existe
+
+		var hash string
+
+		db.QueryRow("CALL sp_autenticarUsuario(?)", autenticar.Email).Scan(&hash)
+
+		err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(autenticar.Password))
+
+		if err == nil { //pasword correcto
+
+			var id, nombreCompleto, rol string
+
+			db.QueryRow("CALL sp_autenticacionCorrecta(?)", autenticar.Email).Scan(&id, &nombreCompleto, &rol)
+
+			fmt.Println(id, nombreCompleto, rol)
+
+			token := app.GenerateTokenPaseto(id, rol)
+
+			c.JSON(http.StatusOK, gin.H{
+				"token":          token,
+				"id":             id,
+				"nombreCompleto": nombreCompleto,
+			})
+
+		} else {
+
+			//retornamos el error
+			c.JSON(http.StatusUnauthorized,
+				gin.H{"error": "Error en el usuario o contraseña"},
+			)
+
+		}
+
+	} else { //si email si existe
 
 		c.JSON(http.StatusConflict, gin.H{
 			"message": "Email no existe",
 		})
 
 		return
-
-	} else { //si email si existe
 
 	}
 
